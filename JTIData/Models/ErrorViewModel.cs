b5008b0370e28e88491f634408e81e352a5c37ce
jaherrada <43144061+jaherrada@users.noreply.github.com>
2019-01-14 23:42:42 -0500

    Agregar archivos de proyecto.

---
+++ JTIData/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using JTIData.Models;
+
+namespace JTIData.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
